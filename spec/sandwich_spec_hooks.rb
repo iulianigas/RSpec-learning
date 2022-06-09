df324540c9813a1cb3addf374d907a9b3eac8fee
Iulian Igas <41596757+iulianigas@users.noreply.github.com>
2022-06-09 21:20:34 +0300

    Added first RSpec tests

---
+++ spec/sandwich_spec_hooks.rb
@@ -0,0 +1,19 @@
+#learn about hooks
+Sandwich = Struct.new(:taste, :toppings)
+
+RSpec.describe 'An ideal sandwich' do 
+  before { @sandwich = Sandwich.new('delicious', []) }  #this is a hook
+
+  it 'is delicious' do 
+    taste = @sandwich.taste
+
+    expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do 
+    @sandwich.toppings << 'cheese'
+    toppings = @sandwich.toppings
+
+    expect(toppings).not_to be_empty
+  end
+end
